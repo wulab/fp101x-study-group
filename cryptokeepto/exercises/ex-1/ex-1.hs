8e530fcf525501a99ddcc345b256f7c42488c0a9
cryptokeepto <cryptokeepto@gmail.com>
2021-10-10 14:46:57 +0700

    Refactor code ex-0 and add new ex-1

---
+++ cryptokeepto/exercises/ex-1/ex-1.hs
@@ -0,0 +1,11 @@
+-- The script below contains syntactic errors. Correct these errors and then choose right answer.
+
+
+-- Try pass one parameter
+-- m a = a `div` length xs
+--   where xs = [1,2,3,4,5]
+
+m = a `div` length xs
+  where a = 10
+        xs = [1,2,3,4,5]
+
