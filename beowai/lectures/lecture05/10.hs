46bba2f700e75826927bf18d0eae07e1d1439fd0
Hatsadin Payappanon <beowai@outlook.com>
2021-12-11 11:02:11 +0700

    Seperate folder

--- beowai/lectures/lecture05/10.hs
+++ beowai/lectures/lecture05/10.hs
@@ -1,6 +1,6 @@
 -- Choose the correct definition for the function msort :: Ord a => [a] -> [a] that implements merge sort, in which the empty list and singleton lists are already sorted, and any other list is sorted by merging together the two lists that result from sorting the two halves of the list separately. The solutions can use the function merge from the previous exercise and the function halve that splits a list into two halves whose lengths differ by at most one.
 
-halve : [a] -> ([a], [a])
+halve :: [a] -> ([a], [a])
 halve xs = splitAt (length xs `div` 2) xs
 
 msort [] = []
